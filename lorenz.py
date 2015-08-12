82dbf2462ab7bfe47bcdcd923d38dca9ae49d565
Ubuntu <ubuntu@ip-172-31-30-94.us-west-2.compute.internal>
2015-08-12 18:24:26 +0000

    Removed integerization

--- lorenz.py
+++ lorenz.py
@@ -16,16 +16,16 @@ def plot(i, x, y, r):
     W, H = 1920, 1080
     y0, y1 = 0, 55
 
-    y = array(H * (y1 - y) / (y1 - y0), int)
-    x = array(H * x / (y1 - y0) + W / 2, int)
-    wavelength = array((r - r[0]) / (r[-1] - r[0]) * (830 - 360) + 360, int)
+    y = H * (y1 - y) / (y1 - y0)
+    x = H * x / (y1 - y0) + W / 2
+    wavelength = (r - r[0]) / (r[-1] - r[0]) * (830 - 360) + 360
 
     f = Frame(filename + '_raw.exr', (W, H))
     f.accumulate(x, y, wavelength)
     f.close()
 
     # os.system('exrnormalize {0}_raw.exr {0}.exr 1'.format(filename))
-    # os.system('exrtopng {0}.exr {0}.png'.format(filename))
+    os.system('exrtopng {0}_raw.exr {0}.png &'.format(filename))
 
 dt = 1. / 4 / 60
 N = 1000000
@@ -34,15 +34,15 @@ y = ones(N)
 z = ones(N) + 28
 r = linspace(27, 29, N)
 
-xyz = array([x, y, z])
+xyz = array([x, y, z], float)
 
-plot(0, xyz[:,0], xyz[:,1], r)
+plot(0, xyz[0], xyz[2], r)
 for iFrame in range(1, 60 * 60 * 5):
     for iStep in range(2):
         f0 = lorenz(xyz, r) * dt
-        f1 = lorenz(xyz + 0.5 * f0) * dt
-        f2 = lorenz(xyz + 0.5 * f1) * dt
-        f3 = lorenz(xyz + f2) * dt
+        f1 = lorenz(xyz + 0.5 * f0, r) * dt
+        f2 = lorenz(xyz + 0.5 * f1, r) * dt
+        f3 = lorenz(xyz + f2, r) * dt
         xyz += (f0 + f3) / 6 + (f1 + f2) / 3
-        plot(iFrame, xyz[:,0], xyz[:,1], r)
+        plot(iFrame, xyz[0], xyz[2], r)
 
